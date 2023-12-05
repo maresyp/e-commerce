5da6a15a0fc2acff9137b977973d5d07a22e521c
maresyp <mattq.2000@icloud.com>
2023-12-05 12:02:56 +0100

    Add Dockerfile and compose.yml, update settings.py for PostgreSQL database configuration

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -85,11 +85,16 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# postgres database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db-web',
+        'PORT': '5432',
+    },
 }
 
 
