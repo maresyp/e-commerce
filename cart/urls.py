e49d82a004a08bb68495e1c9c3dd97681e8bfae9
maresyp <mattq.2000@icloud.com>
2023-12-05 14:42:17 +0100

    Add cart functionality to the navbar

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('cart/', views.cart, name="cart"),
     path('add_product/<uuid:product_id>/<int:quantity>/', views.add_product, name="add_product"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -7,7 +7,24 @@ from shop.models import Product
 
 from .models import CartEntry, ShoppingCart
 
+
 # Create your views here.
+def cart(request):
+    # check if request comes from logged in user
+    if request.user.is_authenticated:
+        user = request.user
+        user_cart, created = ShoppingCart.objects.get_or_create(owner=user)
+    else:
+        # Get the session of the user
+        user_session = request.session
+        if 'cart_id' in user_session:
+            cart_id = user_session['cart_id']
+            user_cart = ShoppingCart.objects.filter(pk=cart_id)
+
+    entries = CartEntry.objects.filter(cart__in=user_cart)
+    context = {'entries': entries}
+
+    return render(request, 'cart/cart.html', context)
 
 def add_product(request, product_id, quantity: int):
     product = get_object_or_404(Product, pk=product_id)
