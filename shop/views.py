f83d0095059c424dbc89e4e4bc9a8f183ccf8375
Damian Mirowski <damian_mirowski@wp.pl>
2023-12-09 23:24:03 +0100

    Modify logo and add the category bar.

--- shop/views.py
+++ shop/views.py
@@ -5,8 +5,9 @@ from .models import Product
 # Create your views here.
 
 def store(request):
+    page = 'store'
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'page': page}
 
     return render(request, 'shop/store.html', context)
 
