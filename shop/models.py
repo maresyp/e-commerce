149664dba54c8fb493ccff49e00ce3ad9e19e4be
Damian Mirowski <damian_mirowski@wp.pl>
2023-12-13 00:29:56 +0100

    Added cateogries models.

--- shop/models.py
+++ shop/models.py
@@ -2,15 +2,22 @@ import uuid
 
 from django.db import models
 
-# Create your models here.
+class MainCategory(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    name = models.CharField(max_length=50)
 
+class SubCategory(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
+    mainCategory = models.ForeignKey("shop.MainCategory", on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+ 
 class Product(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     quantity = models.IntegerField()
     description = models.TextField()
-    product_image = models.ImageField(upload_to='shop/images', default="", null=True, blank=True)
+    product_image = models.ImageField(upload_to='shop', default="", null=True, blank=True)
 
     def __str__(self):
         return self.name
