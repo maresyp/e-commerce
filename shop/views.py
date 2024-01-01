cecc29f0f52c53b2bc03f62fc31b052525bf61dd
Damian Mirowski <damian_mirowski@wp.pl>
2024-01-01 22:27:40 +0100

    Subcategory filtering added.

--- shop/views.py
+++ shop/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
-
-from .models import Product, MainCategory
-
-
-# Create your views here.
+from .models import MainCategory, SubCategory, Product
 
 def store(request, category_id=None):
     page = 'store'
     categories = MainCategory.objects.all()
-    
+
+    # Filtrowanie produktów na podstawie kategorii głównej
     if category_id:
         products = Product.objects.filter(category__mainCategory__id=category_id)
     else:
         products = Product.objects.all()
-    context = {'products': products, 'page': page, 'categories': categories}
+
+    # Filtrowanie na podstawie zapytania wyszukiwania
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
+
+    # Grupowanie podkategorii według głównych kategorii
+    category_subcategories = {}
+    for category in categories:
+        category_subcategories[category] = SubCategory.objects.filter(mainCategory=category).distinct()
+
+    context = {
+        'products': products,
+        'page': page,
+        'categories': categories,
+        'category_subcategories': category_subcategories,
+        'category_id': category_id
+    }
 
     return render(request, 'shop/store.html', context)
-
