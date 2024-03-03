417845b64ef4582d99c322c7ebd7de699a634d05
maresyp <mattq.2000@icloud.com>
2024-03-03 21:49:18 +0100

    Add API endpoints for retrieving products by category and all categories

--- shop/api/urls.py
+++ shop/api/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("get_all_products/", views.get_all_products),
     path("get_product_image/<uuid:product_id>", views.get_product_image),
+    path("get_products_with_category/<str:category>", views.get_products_with_category),
+    path("get_all_categories/", views.get_all_categories),
 ]
