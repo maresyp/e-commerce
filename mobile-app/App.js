ad97bfd0c85481dede2e349764c790ff3c4b21bf
Damian Mirowski <damian_mirowski@wp.pl>
2024-03-23 20:24:15 +0100

    Logo added

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BottomTabNavigator from './components/navigator';
 import { NavigationContainer } from '@react-navigation/native';
@@ -10,10 +10,13 @@ export default function App() {
   return (
     <NavigationContainer>
       <SafeAreaView style={{ flex: 1 }}>
+        <Image
+          source={{ uri: 'http://10.0.2.2:8000/static/images/logo.png' }}
+          style={styles.logo}
+        />
         <View style={styles.header}>
-          <Text>Tutaj będzie logo</Text>
         </View>
-        <BottomTabNavigator />
+        <BottomTabNavigator/>
       </SafeAreaView>
     </NavigationContainer>
   );
@@ -21,6 +24,15 @@ export default function App() {
 
 const styles = StyleSheet.create({
   header: {
-    height:'5%',
+    height:'7%',
+    overflow: 'visible',
   },
+  logo: {
+    position: 'absolute', // Pozycjonowanie absolutne
+    top: 30, // Odległość od górnej krawędzi
+    left: '10%', // Odległość od lewej krawędzi
+    width: 120, // Szerokość logo
+    height: 70, // Wysokość logo
+    zIndex: 1,
+  }
 });
