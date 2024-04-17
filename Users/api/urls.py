7ad0d271a8fc21c0fc256ceb641a8819d2581702
maresyp <mattq.2000@icloud.com>
2024-04-17 18:16:39 +0200

    Add change_password endpoint to User API

--- Users/api/urls.py
+++ Users/api/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("register/", views.register_user, name="register_user"),
+    path("change_password/", views.change_password, name="change_password"),
 ]
