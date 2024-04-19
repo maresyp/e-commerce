8874056d5fd2e0d07dea2b02649baf19e3665cbd
Damian Mirowski <damian_mirowski@wp.pl>
2024-04-19 23:46:00 +0200

    Vercel configuration

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '10.0.2.2', '.vercel.app']
 
 
 # Application definition
