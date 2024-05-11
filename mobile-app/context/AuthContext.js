6a63bafe7c93de63581312adcd21c9e5837df728
Damian Mirowski <damian_mirowski@wp.pl>
2024-05-11 13:59:05 +0200

    Fixed navigation after logout.

--- mobile-app/context/AuthContext.js
+++ mobile-app/context/AuthContext.js
@@ -24,7 +24,7 @@ export const AuthProvider = ({ children }) => {
     let [user, setUser] = useState(null)
     let [loading, setLoading] = useState(true)
 
-    const navigation = useNavigation()
+    const navigation = useNavigation();
     const apiUrl = Platform.OS === 'ios' ? 'http://127.0.0.1:8000/api/' : 'http://10.0.2.2:8000/api/';
 
     useEffect(() => {
@@ -117,7 +117,7 @@ export const AuthProvider = ({ children }) => {
 
     let logoutUser = () => {
         clearUserData()
-        navigation.navigate('Home')
+        navigation.navigate('HomeStack')
         return true
     }
 
